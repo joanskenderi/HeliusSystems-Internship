8f52aaa925acfaebd76eade6d5ee0938a0bcd611
joanskenderi <jskenderi@outlook.com>
2022-04-01 00:49:40 +0200

    kot

---
+++ Projects/02 - JavaScript & jQuery/app.js
@@ -0,0 +1,36 @@
+// CHECKIN / CHECKOUT DATES POP OUT
+$(function () {
+  $("#checkin-date").hide();
+  $("#checkout-date").hide();
+
+  $("input[name=way]:radio").click(function () {
+    if ($("input[name=way]:checked").val() == "1") {
+      $("#checkout-date").hide();
+      $("#checkin-date").show();
+    } else if ($("input[name=way]:checked").val() == "2") {
+      $("#checkin-date").show();
+      $("#checkout-date").show();
+    }
+  });
+});
+
+// VALIDATION
+
+// Name Validation
+$("#fname").on("blur", function () {
+  if ($(this).val().match("^[a-zA-Z]{3,16}$")) {
+    alert("Valid name");
+  } else {
+    alert("That's not a name");
+  }
+});
+
+// Email Validation
+function validateEmail($email) {
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  return emailReg.test($email);
+}
+
+// Phone Number Validation
+
+// border i kuq if error

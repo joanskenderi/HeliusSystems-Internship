55e09bbc1453f2dbc7a8a679a68112c9a36d4eb0
joanskenderi <jskenderi@outlook.com>
2022-04-21 01:18:59 +0200

    JS Commit

--- 03 - JavaScript/Day1.js
+++ 03 - JavaScript/Day1.js
@@ -185,6 +185,144 @@ if (a === b) {
   alert("jo");
 }
 
-// funx te string: concat / includes / indexof /split /slice
+// Function that adds 2 numbers and returns the sum
+function sum(firstNumber, secondNumber) {
+  var s = firstNumber + secondNumber;
+  return s;
+}
 
-// kot per test 🔥
+// Mblidh numrat e plote
+function sum(firstNumber, secondNumber) {
+  var a = parseInt(firstNumber);
+  var b = parseInt(secondNumber);
+  var s = a + b;
+  return s;
+}
+
+// Zbrit nga numri me i madh ate me te vogel
+function difference(firstNumber, secondNumber) {
+  var a = 0;
+  if (firstNumber > secondNumber) {
+    a = firstNumber - secondNumber;
+  } else {
+    a = secondNumber - firstNumber;
+  }
+  return a;
+}
+
+// Arrow functions
+const difference = (firstNumber, secondNumber) => {
+  var a = 0;
+  if (firstNumber > secondNumber) {
+    a = firstNumber - secondNumber;
+  } else {
+    a = secondNumber - firstNumber;
+  }
+  return a;
+};
+
+// Arrays (tipe te ndryshme te dhenash ne nje array)
+const sum = (a, b) => {
+  return a + b;
+};
+
+var arrayTest = [17, "Joan", sum];
+console.log(arrayTest);
+
+// Menyre tjeter per te krijuar array
+var anotherArray = new Array(2002, "Skenderi", sum);
+console.log(anotherArray);
+
+//
+var arrayTest = [
+  { firstName: "Joan", secondName: "Skenderi", age: 20 },
+  { firstName: "John", secondName: "Doe", age: 31 },
+  { firstName: "Lorem", secondName: "Ipsum", age: 27 },
+];
+
+console.log(arrayTest.includes("o"));
+
+//
+var number = 5;
+function sum(firstNumber, secondNumber) {
+  var a = 0;
+  number + a;
+}
+sum;
+console.log(number); // variabel i deklaruar globalisht
+
+// object
+var objectPerson = {
+  firstName: "Joan",
+  secondName: "Skenderi",
+  min: 5,
+  max: 10,
+};
+
+function difference() {
+  return max - min;
+}
+difference;
+console.log(objectPerson);
+
+var objectTest = new Object();
+objectTest.firstName = "Joan"; // per te shtuar properties
+
+/*
+Te ndertohet nje klase me emrin Student, e cila permban info ne lidhje me emrin, mbiemrin, degen dhe vitin e studimit.
+Kjo klase ruan gjithashtu info per te gjitha notat e studentit.
+Emri, mbiemri, dega dhe viti i studimit mbushen duke perdorur nje konstruktor.
+Per te shtuar nota klasa perdor nje metode me emrtimin @grade.
+Klasa ka po ashtu nje metode(funx) qe kthen emrin e plote te studentit.
+Ndertoni klasen dhe krijoni 4 instanca te ksaj klase.
+Per te gjithe studentet e krijuar te dhenat kalojini ne nj vektor.
+Afishoni emrin e plote dhe noten max te gjithe studenteve.
+Me konstruktor mbushen vetem 4 props e para. notat i mbush me funx (shton notat te vektori notave duke perdorur komandat qe mesuam sot.)
+*/
+
+class Student {
+  constructor(firstName, lastName, dega, viti) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dega = dega;
+    this.viti = viti;
+    this.notat = [];
+  }
+  
+  set addGrade = (grade) => { //set do vendosesh nk ndikon ne result
+    this.notat.push(grade)
+    }
+  get emriPlote = () => {
+    return this.firstName + " " + this.secondName
+  }
+
+  var st1=new Student("Mel","Doko","IE",1)
+    var st2=new Student("Fl","tt","fin",3)
+    var st3=new Student("ab","yt","ekon",1)
+    var st4=new Student("tr","zt","adm",2)
+    st1.addGrade(10)
+    st1.addGrade(8)
+    st1.addGrade(6)
+    st1.addGrade(9)
+    st2.addGrade(10)
+    st2.addGrade(8)
+    st2.addGrade(6)
+    st2.addGrade(9)
+    st3.addGrade(7)
+    st3.addGrade(9)
+    st3.addGrade(5)
+    st3.addGrade(9)
+    st4.addGrade(4)
+    st4.addGrade(8)
+    st4.addGrade(6)
+    st4.addGrade(7)
+    var st=new Array(st1,st2,st3,st4)
+    st.forEach ((el,idx) => {
+    console.log(el.emriPlote())
+    var max=el.notat[0]
+    for(var i=1;i<notat.length;i++)
+    {if(el.notat[i]>max)
+    max=el.notat[i]
+    }
+    })
+}

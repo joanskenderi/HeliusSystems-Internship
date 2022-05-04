dbe79a3b944dcd7f5dfa7a7ca7e3e8636d5d8665
joanskenderi <jskenderi@outlook.com>
2022-05-04 18:02:43 +0200

    C#  Commit🔥

--- 07 - C#/02 - Name  and Age/NameAge/NameAge/Program.cs
+++ 07 - C#/02 - Name  and Age/NameAge/NameAge/Program.cs
@@ -5,9 +5,9 @@ namespace NameAge
     class Program
     {
         static void Main(string[] args)
-        {
-            int age;
+        {   
             string name;
+            int age;
 
             Console.WriteLine("Enter your name: ");
             name = Console.ReadLine();
